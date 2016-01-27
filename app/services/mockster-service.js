4ccfd7ad053ce31b9abc57debf75154f5900c1a2
Dominic Waterson <dominic.waterson@tombola.com>
2016-01-27 16:43:17 +0000

    made some progress on polling routes

--- app/services/mockster-service.js
+++ app/services/mockster-service.js
@@ -2,15 +2,16 @@
     'use strict';
     angular.module('Tombola.Mockster')
 		.service('MocksterService', ['$rootScope', function ($rootScope) {
+			var pollingCount = 0;
+			var caughtup = false;
 			var express = require('express');
 			var bodyParser = require('body-parser');
+			//var xmlparser = require('express-xml-bodyparser');
 			var myApp = express();
 			var fs = require('fs');
 			var me = this;
 			me.port = '';
 			me.requests = [];
-
-
 
 			myApp.use(function(req, res, next) {
 				res.header('Access-Control-Allow-Origin', '*');
@@ -20,7 +21,10 @@
 				next();
 			});
 
-			myApp.use(bodyParser.urlencoded({ extended: false }));
+			//myApp.use(bodyParser.urlencoded({ extended: false }));
+			//TODO important!!!!!!!
+			myApp.use(bodyParser.text({type: '*/*'}));
+			//myApp.use(xmlparser());
 
 			myApp.use(function(req,res, next){
 				var actualEnd = res.end;
@@ -58,80 +62,45 @@
 				res.send('Hello GET');
 			});
 
-			//TODO localhost:8081/bingo75/connect?server=dev-bingo75.tombola.co.uk:12353&bust=1453471957877
 			myApp.get('/:game/connect', function (req, res) {
 				//TODO: sort file handling - need to build/package repsonses
 				//TODO: strategy for loading responses - do we really want a json.parse???
-				//obj = fs.readFileSync('/games/bingo75/connect.json', 'utf8');
-				console.log('Connect');
 				var obj = JSON.parse('{ "guid": "bf00d6b2-0a7b-4b14-b66f-2c507bbc7108" }');
 				res.send(obj);
 			});
 
 			myApp.get("/:game/disconnect", function (req, res) {
-				console.log('Disconnect');
-				res.send(true);
+				res.send('{ "disconnected": "true" }');
 			});
 
 			myApp.post('/:game/send', function (req, res) {
-				console.log(req.body);
 				//TODO: reqno is coming oup as string.
-				if(req.query.reqno == 1){
-					var foo = '{\"Number\":1,\"Contents\":\"<connected />\"}';
-					var obj = JSON.parse(foo);
-					console.log("Is one 1");
-					res.send(obj);
-				} else {
-					console.log("Not 1");
+				if(req.body.match(/command="auth"/g)){
+					//caughtup = true;
+					var obj1 = JSON.parse('[{\"Number\":2,\"Contents\":\"<admin command=\\\"auth\\\" user=\\\"waterson89\\\" balance=\\\"996977\\\" existingspend=\\\"0\\\" nextgameq=\\\"0\\\" club=\\\"1\\\" nextgameclub=\\\"0\\\" currentgameclub=\\\"0\\\"  ticketprice=\\\"25\\\" pattern=\\\"0111011111101011111110001\\\" patternname=\\\"Space Invader\\\" merged=\\\"false\\\" />\"},{\"Number\":6,\"Contents\":\"<calls command=\\\"catchup\\\" gameno=\\\"981580\\\" gamestage=\\\"1\\\" quantity=\\\"66\\\" pattern=\\\"1010100000101010000010101\\\" patternname=\\\"Grid\\\" promotype=\\\"0\\\" houseprize=\\\"0\\\" jackpot=\\\"50000\\\" jackpotwonon=\\\"42\\\" timeelapsed=\\\"273772.1873\\\"  noplayers=\\\"0\\\" calls=\\\"016557321806636712584226111662445373454315703638286975487460310851023403504168252366597220334707614664711421170513301024225204094035\\\" />\"}]');
+					res.send(obj1);
+				}
+				else if(req.body.match(/command="clubcount"/g)){
 					var obj1 = JSON.parse('{\"Number\":2,\"Contents\":\"<admin command=\\\"auth\\\" user=\\\"waterson89\\\" balance=\\\"996977\\\" existingspend=\\\"0\\\" nextgameq=\\\"0\\\" club=\\\"1\\\" nextgameclub=\\\"0\\\" currentgameclub=\\\"0\\\"  ticketprice=\\\"25\\\" pattern=\\\"0111011111101011111110001\\\" patternname=\\\"Space Invader\\\"  />\"}');
 					res.send(obj1);
 				}
+				else if(req.body.match(/command="purchase"/g)){
+					var obj1 = JSON.parse('[{\"Number\":8,\"Contents\":\"<admin command=\\\"purchase\\\" success=\\\"true\\\" newbalance=\\\"996967\\\" newexistingspend=\\\"10\\\" quantity=\\\"1\\\" />\"},{\"Number\":9,\"Contents\":\"<cards game=\\\"next\\\" quantity=\\\"1\\\" gameno=\\\"981581\\\"><ticket id=\\\"A\\\" numbers=\\\"08244154750718326065142643486709194256631523345370\\\" /></cards>\"}]');
+					res.send(obj1);
+				}
+				else if(req.query.reqno == 1 && !caughtup) {
+					var foo = '{\"Number\":1,\"Contents\":\"<connected />\"}';
+					var obj = JSON.parse(foo);
+					res.send(obj);
+				}
+				//else if (caughtup) {
+				//	if (pollingCount === 0) {
+				//		pollingCount++;
+				//		var obj3 = JSON.parse('{\"Number\":7,\"Contents\":\"<calls command=\\\"calls\\\" callno=\\\"67\\\" call=\\\"54\\\" />\"}');
+				//		res.send(obj3);
+				//	}
+				//}
 			});
-
-
-			//TODO http://{proxy-url}/disconnect?id={guid}... responses with A Boolean value - true is disconnected, false if not.
-			//myApp.get('/:game/disconnect/:', function (req, res) {
-			//	var obj;
-			//	obj = fs.readFileSync(__dirname + "/games/" + req.params.game + "/" + req.params.game + '.json', 'utf8');
-			//	obj = JSON.parse(obj);
-			//	res.send(obj.connect);
-			//});
-
-
-			// POSTS auth http://{proxy-url}/send?id={guid}&message={messageNumber}&reqno={requestNumber} <Admin Command="auth" ClubNo="{clubNumber}" siteid="{siteID}" id="{id}" clienttypeid="{clientTypeID}" CrossCheck="{crossCheck}"/>
-
-
-			// purchase http://{proxy-url}/send?id={guid}&message={messageNumber}&reqno={requestNumber}  <Admin Command="purchase" existingamount="{existingAmount}" gameno="{gameNumber}" id="{id}" amount="{amount}" quantity="{quantity}" pattern="{pattern}"/>
-
-
-
-			//myApp.get('/connect/:game', function (req, res) {
-			//	var obj;
-			//	obj = fs.readFileSync("/games/" + req.params.game + "/" + req.params.game + '.json', 'utf8');
-			//	obj = JSON.parse(obj);
-			//	res.send(obj.connect);
-			//});
-            //
-			//myApp.post('/send/:game/auth', function (req, res) {
-			//	var obj;
-			//	obj = fs.readFileSync("/games/" + req.params.game + "/" + req.params.game + '.json', 'utf8');
-			//	obj = JSON.parse(obj);
-			//	res.send(obj.auth);
-			//});
-            //
-			//myApp.post('/send/:game/purchase', function (req, res) {
-			//	var obj;
-			//	obj = fs.readFileSync("/games/" + req.params.game + "/" + req.params.game + '.json', 'utf8');
-			//	obj = JSON.parse(obj);
-			//	res.send(obj.purchase);
-			//});
-            //
-			//myApp.post('/send/:game/:number', function (req, res) {
-			//	var obj;
-			//	obj = fs.readFileSync("/games/" + req.params.game + "/" + req.params.game + '.json', 'utf8');
-			//	obj = JSON.parse(obj);
-			//	res.send(obj.calls[req.params.number - 1]);
-			//});
 
 			me.start = function(){
 				var server = myApp.listen(8081, function () {
